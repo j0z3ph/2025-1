146101e6a75de8ac4f839da658447e663a17d249
Jose Luis Cruz <j0z3ph@gmail.com>
2024-09-04 20:26:46 -0600

    Primer programa

---
+++ IP/P1/programa.c
@@ -0,0 +1,18 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C
+ * @version 0.1
+ * @date 2024-09-04
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    /* code */
+    return 0;
+}
+
