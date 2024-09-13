33c12b150cf2a2578a16ae6b7a4ea5a4139a49a7
Jose Luis Cruz <j0z3ph@gmail.com>
2024-09-12 21:46:52 -0600

    Clases

---
+++ PA/P3/abstractos.py
@@ -0,0 +1,12 @@
+class Figura:
+    def area(sefl):
+        raise NotImplementedError()
+    
+class Cuadrado(Figura):
+    pass
+
+c1 = Cuadrado()
+
+
+#print(c1.area())
+circulo = Figura()
--- PA/P3/programa.py
+++ PA/P3/programa.py
@@ -2,13 +2,31 @@
 date: Hoy mesmo
 Un programa
 """
+from multimethod import multimethod
 
 class Persona:
     de_clase = "Soy un cacahuate"
     
     def __init__(self, nombre:str, edad:int) -> None:
-        self.nombre = nombre
-        self.edad = edad
+        self.__nombre = nombre
+        self.__edad = edad
+    
+    @property    
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, name: str):
+        self.__nombre = name
+    
+    @property    
+    def edad(self):
+        return self.__edad
+    
+    @edad.setter
+    def edad(self, edad:int):
+        self.__edad = edad
+        
     
     @classmethod
     def metodo_de_clase(cls):
@@ -19,14 +37,37 @@ class Persona:
         pass
         
     def __str__(self) -> str:
-        return f"Hola, me llamo {self.nombre} y tengo {self.edad} años."
+        return f"Hola, me llamo {self.__nombre} y tengo {self.__edad} años."
     
+    @multimethod
     def saluda(self):
-        print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años.")
+        print(f"Hola, me llamo {self.__nombre} y tengo {self.__edad} años.")
+
+    @multimethod
+    def saluda(self, msg:str):
+        print(msg)
+        
+    def __len__(self):
+        return 10
+    
+    def __add__(self, obj):
+        return self.__edad + obj.edad
+
+class Alumno(Persona):
+    pass
 
 entero = 10
 
-pipol = Persona("",0)
+julio = Alumno("",0)
+julio.edad = 10
+print(julio.edad)
+
+pipol = Persona("",10)
+
 Persona.metodo_de_clase()
 #print(pipol)
-pipol.saluda()
+pipol.nombre = "Jose"
+print(pipol.nombre)
+pipol.saluda("Un mensajito")
+
+print(pipol + julio)
