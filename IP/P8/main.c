7ed458be95b992e2a19ef86bd7478173f672c016
Jose Luis Cruz <j0z3ph@gmail.com>
2024-09-24 20:22:41 -0600

    Ejemplos

---
+++ IP/P8/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief 
+ * @version 0.1
+ * @date 2024-09-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    int variable = 0b11111111111111111111111111111111;
+
+    printf("%d\n", variable);
+    
+    return 0;
+}
