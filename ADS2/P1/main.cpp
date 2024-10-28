bae4b2031c60dc6c06a467663310e0a9245be9c1
Jose Luis Cruz <j0z3ph@gmail.com>
2024-10-28 17:20:33 -0600

    Primer programa

---
+++ ADS2/P1/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C++ :>
+ * @version 0.1
+ * @date 2024-10-28
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <iostream>
+#define CONSTANTE2 10
+
+using namespace std;
+
+int main()
+{
+    int variable = 10;
+    string nombre;
+
+    cout << "Usuario, escribe un numero entero positivo: ";
+    cin >> variable;
+    cout << "El cuadrado de " << variable << " es "
+         << variable * variable << "." << endl;
+    getc(stdin);
+    getline(cin, nombre);
+    cout << nombre;
+
+
+
+    return 0;
+}
