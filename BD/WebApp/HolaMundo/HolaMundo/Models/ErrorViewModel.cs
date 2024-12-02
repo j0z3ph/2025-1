e9e7790be08dbbddfc3298b740931483c3ee0970
Jose Luis Cruz <j0z3ph@gmail.com>
2024-12-02 15:58:22 -0600

    Mi primer web app

---
+++ BD/WebApp/HolaMundo/HolaMundo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HolaMundo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
