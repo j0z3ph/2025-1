8fff77f6db3d4b71b7a8767f28cb19e23b8026f8
Jose Luis Cruz <j0z3ph@gmail.com>
2024-12-04 16:04:16 -0600

    Update Example2Context.cs

--- BD/WebApp/HolaMundo/HolaMundo/Models/Example2Context.cs
+++ BD/WebApp/HolaMundo/HolaMundo/Models/Example2Context.cs
@@ -23,10 +23,6 @@ public partial class Example2Context : DbContext
 
     public virtual DbSet<Sale> Sales { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=18.117.9.177;Database=EXAMPLE2;User Id=TestApp;Password=yqekAw59CVv7MRFXbtnSrYhxaEmG24s6cDQ8NpdH3gfWjBKPZz;Encrypt=True;TrustServerCertificate=True;Trusted_Connection=False;Integrated Security=False;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("Latin1_General_CI_AI");
